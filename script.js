36654b5d9d8408c1adeb3ab0a2b5dc35c0b2efad
Fernando <feer.rodriguez96@gmail.com>
2021-08-21 19:26:35 -0300

    #1 dev - Boton completar
    Se ha añadido funcionalidad al boton limpiar completados.
    
    ClearCompletedButton

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ var addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addToDoItem);
 
 function addToDoItem() {
-    alert ("Boton de añadir clickeado!")
+    alert ("Boton de anadir clickeado!")
+}
+
+var clearCompletedButton = document.getElementById("clear-completed-button");
+clearCompletedButton.addEventListener("click", clearCompeltedToDoItems);
+
+function clearCompeltedToDoItems(){
+    alert ("Se han limpiado los elementos completados")
 }
